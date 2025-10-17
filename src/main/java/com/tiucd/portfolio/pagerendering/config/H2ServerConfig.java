1d9de3379830a043cf9d24868c3063b380c4e8f5
webcollab08 <webcollab.08@gmail.com>
2025-10-17 11:47:11 -0700

    change the h2 to oracle

---
+++ src/main/java/com/tiucd/portfolio/pagerendering/config/H2ServerConfig.java
@@ -0,0 +1,18 @@
+package com.tiucd.portfolio.pagerendering.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.SQLException;
+
+@Configuration
+@Profile("!production")
+public class H2ServerConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+}
